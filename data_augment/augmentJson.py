e3d1b0932211e1074215bc7c86694ace1b1d26d6
구민구 <corleone@kakao.com>
2021-07-28 19:18:04 +0900

    Create augmentJson.py

---
+++ data_augment/augmentJson.py
@@ -0,0 +1,30 @@
+import json
+
+def augment_json_data(file_name, method="h", path="./"):
+    # read json
+    with open(f"{path}{file_name}", 'r') as f:
+        json_data = json.load(f)
+
+    # get image scale
+    width = json_data['label_info']['image']['width']
+    height = json_data['label_info']['image']['height']
+
+    # new points
+    new_points = []
+
+    # augment data
+    if method == "h":
+        for i in range(len(json_data['label_info']['shapes'])):
+            new_points.append([[width - point[0], point[1]] for point in json_data['label_info']['shapes'][i]['points']])
+    elif method == "v":
+        for i in range(len(json_data['label_info']['shapes'])):
+            new_points.append([[point[0], height - point[1]] for point in json_data['label_info']['shapes'][i]['points']])
+        
+    for i in range(len(new_points)):
+        json_data['label_info']['shapes'][i]['points'] = new_points[i]
+    
+    # create new Json file
+    with open(f'{path}{file_name}-{method}', 'w') as f:
+        json.dump(json_data, f)
+
+    return json_data
