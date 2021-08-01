6f60caec05001641bfbf0161602e756ab9f849e2
구민구 <corleone@kakao.com>
2021-08-02 00:35:59 +0900

    Update augmentDataSet.py

--- data_augment/augmentDataSet.py
+++ data_augment/augmentDataSet.py
@@ -6,17 +6,23 @@ import glob, os
 from augmentImage import *
 from augmentJson import *
 
-source_dir = "D:/data_set/meat_data/test/"
-target_dir = "D:/data_set/meat_data/test/"
+source_dir = "D:/data_set/meat_data/training/"
+target_dir = "D:/data_set/meat_data/training/"
 
 image_augmentor(source_dir, target_dir, v=True, h=True, r=False)
 
+'''
 os.chdir(source_dir)
+count = 1
 for json_file in glob.glob("*.json"):
     augment_json_data(json_file, method="h", path="./")
     augment_json_data(json_file, method="v", path="./")
+    count+=1
 
+    if count % 3000 == 0 :
+        print(count)
 
+'''
 # image_augmentor(input_path, v=True, h=True, r=True)
 # augment_json_data(file_name, method="h", path="./")
 
